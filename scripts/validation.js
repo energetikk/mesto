adbe26110ebfac470cd8300344d35ace1bbc30c8
Pavel Demin <pavlikdemin@yandex.ru>
2023-02-08 01:05:45 +0300

    add feature: create input_error

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,15 +1,35 @@
-const enableValidationConfig = {
-    formSelector: '.popup__form',
-    inputSelector: '.popup__input',
-    submitButtonSelector: '.popup__button',
-    inactiveButtonClass: 'popup__button_disabled',
-    inputErrorClass: 'popup__input_type_error',
+const validationConfig = {
+    formSelector: '.form',
+    inputSelector: '.form__item',
+    submitButtonSelector: '.form__submit',
+    inactiveButtonClass: 'form__submit_disabled',
+    inputErrorClass: 'form__item_type_error',
     errorClass: 'popup__error_visible'
 }
 
 
-function enableValidation () {
-  const form = document.querySelector('.form');
+function enableValidation (config) {
+  const form = document.querySelector(config.formSelector);
   console.log(form);
+  addInputListener(form, config);
 }
-enableValidation();
+enableValidation(validationConfig);
+
+
+
+
+function addInputListener(form, config) {
+  const inputItem = Array.from(document.querySelectorAll(config.inputSelector));
+  inputItem.forEach((item) => {
+    item.addEventListener('input', function(evt) {
+      console.log(evt.target.validity.valid);
+      if (!evt.target.validity.valid) {
+        item.classList.add(config.inputErrorClass);
+      } else {
+        item.classList.remove(config.inputErrorClass);
+      }
+    });
+  })
+  console.log(inputItem);
+}
+
